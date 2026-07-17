15fbfe9c5ca82303a8639d42d2f0377a40a698aa
aditya <adityasahasrabuddhe747@gmail.com>
2026-07-17 08:59:19 +0100

    Stage 2 popups modified.

--- app.py
+++ app.py
@@ -8,16 +8,27 @@ flight_map = folium.Map(location=[47.5, 0.5], zoom_start=5, tiles="CartoDB posit
 with open("flights.json", "r") as f:
     flight_data = json.load(f)
 
+with open("aircrafts.json", "r") as g:
+    aircraft_data = json.load(g)
+
 for flight in flight_data:
     lat1 = flight["route"][0]["latitude"]
     lon1 = flight["route"][0]["longitude"]
     location1 = [lat1, lon1]
-    folium.Marker(location=location1,popup=f"Flight: {flight['flight_number']}",icon=folium.Icon(color="green", icon="plane", prefix="fa")).add_to(flight_map)
+
+    for aircraft in aircraft_data:
+        if flight["registration"] == aircraft["registration"]:
+            folium.Marker(location=location1,popup=f"Flight: {flight['flight_number']}\nAircraft: {aircraft['model']}\nAirline: {aircraft['airline']}",icon=folium.Icon(color="green", icon="plane", prefix="fa")).add_to(flight_map)
+            break
 
     lat2 = flight["route"][-1]["latitude"]
     lon2 = flight["route"][-1]["longitude"]
     location2 = [lat2, lon2]
-    folium.Marker(location=location2,popup=f"Flight: {flight['flight_number']}",icon=folium.Icon(color="blue", icon="plane", prefix="fa")).add_to(flight_map)
+    
+    for aircraft in aircraft_data:
+        if flight["registration"] == aircraft["registration"]:
+            folium.Marker(location=location2,popup=f"Flight: {flight['flight_number']}\nAircraft: {aircraft['model']}\nAirline: {aircraft['airline']}",icon=folium.Icon(color="red", icon="plane", prefix="fa")).add_to(flight_map)
+            break
 
     coordinates = []
     for point in flight["route"]:
